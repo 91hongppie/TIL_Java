34d0c7f2d0567cfacb29cc403d15348d1b189392
91hongppie <91hongppie@gmail.com>
2021-11-19 17:55:35 +0900

    211119 modifiedAppConfig

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -32,11 +32,9 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
-
-
-
 }
+
+
 
